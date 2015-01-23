19c47d0ef11ce463e46d3a5aeade467e5a92105f
jonasverschueren <jonas.verschueren10@imperial.ac.uk>
2015-01-23 14:51:24 +0000

    Changed the firsttime code. Deleted alise experiments

--- alise.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(){
-	std::cout<<"Alise (Jonas really) says hello"<<std::endl;
-	std::cout<<"Minor change to the master branch"<<std::endl;
-}
--- firsttime.cpp
+++ firsttime.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
-// Addition function added
-double add (double b, double c){return (b+c);}
 int main(){
 	std::cout<<"Hello, this is the main branch"<<std::endl;
-	std::cout<<"Hello Jonas"<<std::endl;
-	std::cout<<"Hello Alise"<<std::endl;
 }
