76a1c2adbf7671a266d9627394069306b407de96
alisevii <alise.virbule@gmail.com>
2015-01-23 13:23:07 +0000

    Changed firsttime.cpp

--- firsttime.cpp
+++ firsttime.cpp
@@ -5,4 +5,5 @@ double add (double b, double c){return (b+c);}
 int main(){
 	std::cout<<"Hello, this is the main branch"<<std::endl;
 	std::cout<<"Hello Jonas"<<std::endl;
+	std::cout<<"Hello Alise"<<std::endl;
 }
